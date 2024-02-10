ed609617410c19701eef6059e8613aabeee8ad33
Kerem Ü <kerem.ullen@pm.me>
2024-02-10 15:00:27 +0300

    animes

--- mal/anime.py
+++ mal/anime.py
@@ -1,7 +1,8 @@
 from mal.mal import get_mal, TOKEN, BASE_URL
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict, Any
 from requests.exceptions import HTTPError
+import csv
 
 
 @dataclass
@@ -35,7 +36,7 @@ class Anime:
     plan_to_watch: int
 
     @classmethod
-    def from_json(cls, json_obj):
+    def from_json(cls, json_obj: Dict[str, Any]):
         return cls(
             id=json_obj['id'],
             title=json_obj['title'],
@@ -43,7 +44,7 @@ class Anime:
             title_en=json_obj['alternative_titles']['en'],
             title_ja=json_obj['alternative_titles']['ja'],
             start_date=json_obj['start_date'],
-            end_date=json_obj['end_date'],
+            end_date=(json_obj['end_date'] if 'end_date' in json_obj else ""),
             synopsis=json_obj['synopsis'],
             mean_score=json_obj['mean'],
             rank=json_obj['rank'],
@@ -65,6 +66,38 @@ class Anime:
             dropped=int(json_obj['statistics']['status']['dropped']),
             plan_to_watch=int(json_obj['statistics']['status']['plan_to_watch']),
         )
+    
+    def to_dict(self) -> Dict[str, Any]:
+        anime = {
+            "id": self.id,
+            "title": self.title,
+            "picture": self.picture,
+            "title_en": self.title_en,
+            "title_ja": self.title_ja,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "synopsis": self.synopsis.replace("\n", " "),
+            "mean_score": self.mean_score,
+            "rank": self.rank,
+            "popularity": self.popularity,
+            "nsfw": self.nsfw,
+            "media_type": self.media_type,
+            "status": self.status,
+            "genres": self.genres,
+            "num_episodes": self.num_episodes,
+            "source": self.source,
+            "average_episode_duration_seconds": self.average_episode_duration_seconds,
+            "rating": self.rating,
+            "studios": self.studios,
+            "num_list_users": self.num_list_users,
+            "num_scoring_users": self.num_scoring_users,
+            "watching": self.watching,
+            "completed": self.completed,
+            "on_hold": self.on_hold,
+            "dropped": self.dropped,
+            "plan_to_watch": self.plan_to_watch,
+        }
+        return anime
 
 
 def get_anime(anime_id: int) -> Anime:
@@ -74,11 +107,76 @@ def get_anime(anime_id: int) -> Anime:
     except HTTPError as err:
         # TODO
         print("HTTP Error: ", err)
-        raise HTTPError
+        raise err
     try:
         anime = Anime.from_json(response)
     except KeyError as err:
         # TODO
         print("Key error: ", err)
-        raise ValueError
+        raise err
     return anime
+
+
+def write_anime_list_to_csv(csv_file: str,
+                            anime_list: List[Anime],
+                            write_headers: bool = False):
+    if write_headers:
+        mode = "w"
+    else:
+        mode = "a"
+    with open(csv_file, mode, newline='', encoding='utf-8') as csv_file:
+        fieldnames = [
+            "id",
+            "title",
+            "picture",
+            "title_en",
+            "title_ja",
+            "start_date",
+            "end_date",
+            "synopsis",
+            "mean_score",
+            "rank",
+            "popularity",
+            "nsfw",
+            "media_type",
+            "status",
+            "genres",
+            "num_episodes",
+            "source",
+            "average_episode_duration_seconds",
+            "rating",
+            "studios",
+            "num_list_users",
+            "num_scoring_users",
+            "watching",
+            "completed",
+            "on_hold",
+            "dropped",
+            "plan_to_watch"
+        ]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        if write_headers:
+            writer.writeheader()
+        for anime in anime_list:
+            row = anime.to_dict()
+            writer.writerow(row)
+
+            
+def gather_animes_and_write_to_csv(csv_file: str,
+                                   start_id: int,
+                                   stop_id: int,
+                                   new_file: bool = False):
+    for id in range(start_id, stop_id):
+        try:
+            anime = get_anime(id)
+        except HTTPError as err:
+            if err.response.status_code == 404:
+                print(f"Anime with ID {id} not found, skipping.")
+                continue
+        if id == start_id and new_file:
+            write_anime_list_to_csv(csv_file=csv_file,
+                                    anime_list=[anime],
+                                    write_headers=True)
+            continue
+        write_anime_list_to_csv(csv_file=csv_file,
+                                anime_list=[anime])
---
+++ scripts/gather_animes.py
@@ -0,0 +1,32 @@
+from mal import anime
+import csv
+import sys
+import os
+
+def continue_id(csv_file: str) -> int:
+   with open(csv_file, "r", newline='', encoding="utf-8") as csvfile:
+       reader = csv.DictReader(csvfile)
+       rows = list(reader)
+       if rows is None:
+           return
+       last_row = rows[-1]
+       return last_row['id']
+
+def main() -> int:
+    filepath = sys.argv[1]
+    if not os.path.exists(filepath):
+        newfile = True
+    else:
+        newfile = False
+    start_id = sys.argv[2]
+    stop_id = sys.argv[3]
+    if start_id == "continue":
+        start_id = continue_id()
+    anime.gather_animes_and_write_to_csv(csv_file=filepath,
+                                         start_id=int(start_id),
+                                         stop_id=int(stop_id),
+                                         new_file=newfile)
+    return 0
+
+if __name__ == "__main__":
+    main()
